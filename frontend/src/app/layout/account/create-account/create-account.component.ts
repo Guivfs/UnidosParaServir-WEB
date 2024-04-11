8ab3d6e30ad98fcf17c748295fb747b387d55a84
Guilherme Vieira <guizinho.vfs@gmail.com>
2024-04-11 18:28:48 -0300

    Cadastro de usuario funcionando pelo frontend

--- frontend/src/app/layout/account/create-account/create-account.component.ts
+++ frontend/src/app/layout/account/create-account/create-account.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from "ngx-toastr";
+import { CreateAccountService } from './create-account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrl: './create-account.component.css'
+  styleUrls: ['./create-account.component.css']
 })
-export class CreateAccountComponent {
+export class CreateAccountComponent implements OnInit {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private createAccountService: CreateAccountService,private route:Router,private toast:ToastrService) {
+    this.form = this.fb.group({
+      nomeCompleto: ['', Validators.required],
+      cep: ['', [Validators.required]],
+      usuario: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.createAccountService.cadastrarUsuario(this.form.value).subscribe(
+        response => {
+          console.log('Dados enviados com sucesso:', response);
+          this.toast.success("Usuário criado com sucesso!")
+          this.route.navigate(['login'])
+        },
+        error => {
+          console.error('Erro ao enviar dados:', error);
+          this.toast.error(error)
+        }
+      );
+    } else {
+      console.error('Formulário inválido. Verifique os campos.');
+    }
+  }
 }
---
+++ frontend/src/app/layout/account/create-account/create-account.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { enviroment } from '../../../../enviroments/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreateAccountService {
+
+  constructor(private http: HttpClient) {}
+
+  cadastrarUsuario(dadosUsuario: any) {
+    const body = {
+      nomeUsuario: dadosUsuario.nomeCompleto,
+      userUsuario: dadosUsuario.usuario,
+      senhaUsuario: dadosUsuario.senha, 
+      cepUsuario: dadosUsuario.cep,
+      emailUsuario: dadosUsuario.email
+    };
+
+    return this.http.post(`${enviroment.baseUrlBackend}/registro`, body);
+  }
+
+}
